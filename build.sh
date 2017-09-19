e489f54c07d00479fe278f4141b9c526da53c850
Deevashwer <deevashwer.student.cse15@iitbhu.ac.in>
2017-09-19 17:55:10 +0530

    Added -py3 flag

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -rf build
 
-swig -c++ -python BV.i #1> /dev/null 2> /dev/null
-python setup.py install #1> /dev/null 2> /dev/null
+swig -c++ -python -py3 BV.i #1> /dev/null 2> /dev/null
+python3 setup.py install #1> /dev/null 2> /dev/null
